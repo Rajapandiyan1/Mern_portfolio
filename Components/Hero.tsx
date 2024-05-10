984a489d4fb6362c89c5f3e222c105fd8ffd6f88
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-05-10 19:08:17 +0530

    remove 90vh

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -13,7 +13,7 @@ function Hero() {
     git:"https://github.com/Rajapandiyan1"
 }
   return (
- <section style={{position:'relative',minHeight:'90vh',alignItems:'center' ,zIndex:"-1"}} className='flex flex-col md:flex-row px-5 py-10 md:py-1  bg-indigo-600 justify-center'>
+ <section style={{position:'relative',alignItems:'center' ,zIndex:"-1"}} className='flex flex-col md:flex-row px-5 py-10 md:py-1  bg-indigo-600 justify-center'>
        <div className='md:w-1/2 flex flex-col'>
             <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> I`m <span className='text-black'>R</span> Rajapandiyan
                 <p className='text-2xl'>I`m MERN Stack Developer</p>
