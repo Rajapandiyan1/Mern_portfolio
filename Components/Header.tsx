8fac0fba25fdbf050161cea91e56f5e2426184eb
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-05-11 11:21:45 +0530

    d-none header

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -16,14 +16,14 @@ const Header = () => {
               <div className="flex-shrink-0">
                   <h2  className="text-white font-bold">Rajapandiyan</h2>
               </div>
-              <div className="block md:hidden">
+              <div className="d-block d-md-none">
                   <button onClick={toggleMenu} type="button" className="text-white hover:text-gray-300 focus:outline-none focus:text-gray-300">
                       <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                       </svg>
                   </button>
               </div>
-              <div className="hidden md:block">
+              <div className="d-none d-md-block">
                   <div className="ml-10 flex items-baseline space-x-4">
                       <Link href="/" className="text-white font-bold text-decoration-none hover:text-gray-300">Home</Link>
                       <Link href="/About" className="text-white font-bold text-decoration-none hover:text-gray-300">About</Link>
@@ -36,7 +36,7 @@ const Header = () => {
       </div>
       {/* Mobile menu, show/hide based on menu state. */}
       {isOpen && (
-          <div className="md:hidden">
+          <div className="d-md-none">
               <div className="px-3 pt-2 pb-3 space-y-1 sm:px-3 flex flex-col">
               <Link href="/" className="text-white font-bold text-decoration-none hover:text-gray-300">Home</Link>
                       <Link href="/About" className="text-white font-bold text-decoration-none hover:text-gray-300">About</Link>
